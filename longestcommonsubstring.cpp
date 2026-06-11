fcf6dec1febe1e7d75d3c4887c36bbc0984c0054
ABHISHEKRMD24 <cabh.it2024@rmd.ac.in>
2026-06-11 07:18:38 +0530

    session-4 RHP Training

---
+++ longestcommonsubstring.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    int n = s1.length();
+    int m = s2.length();
+
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    int ans = 0;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (s1[i - 1] == s2[j - 1]) {
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+                ans = max(ans, dp[i][j]);
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
